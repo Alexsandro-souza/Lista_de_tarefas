ada76a555c5fe8e8bd93bfc9f6407eb120e511d3
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-03-28 20:11:03 -0300

    Adicionando tarefas cegas e botões

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*Captura do elementos HTML*/
+
+const inputTarefas = document.getElementById('input-tarefas');
+const deleteTarefas = document.getElementById('delete-tarefas');
+
+/*const criarTarefa = addEventListener('click', () =>{
+    inputTarefas.value
+});
+const excluirTarefa*/
