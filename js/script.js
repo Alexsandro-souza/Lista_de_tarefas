24d5601b545641806b4d8568a50f61b304ed53f7
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-03-28 23:53:19 -0300

    Evento do click criando tarefa

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-/*Captura do elementos HTML*/
-
+// Captura do elementos HTML
 const inputTarefas = document.getElementById('input-tarefas');
 const deleteTarefas = document.getElementById('delete-tarefas');
+const praFazerList = document.querySelector('.pra-fazer-list');
+const planejarList = document.querySelector('.planejar-list');
+const botaoCriar = document.querySelector('#input-Tarefas');
+const botaoDeletar = document.querySelector('#delete-tarefas');
 
-/*const criarTarefa = addEventListener('click', () =>{
-    inputTarefas.value
+// Evento do click criando tarefa
+const criarTarefa = botaoCriar.addEventListener('click', () =>{
+    console.log('oi')
+    const formTarefa = document.createElement("form");
+    const pTarefa = document.createElement("h3");
+    pTarefa.textContent = 'Informe a nova tarefa aqui abaixo'
+    formTarefa.classList.add("form");
+    const inputTarefa = document.createElement("input");    
+    inputTarefa.type = 'text';    
+    const botaoEnviar = document.createElement("button");
+    botaoEnviar.textContent = 'Criar tarefa';
+    formTarefa.appendChild(pTarefa);
+    formTarefa.appendChild(inputTarefa);
+    formTarefa.appendChild(botaoEnviar);
+    document.body.appendChild(formTarefa);
+
 });
-const excluirTarefa*/
+
+
+// Evento do click excluindo tarefa
+const deletarTarefa = botaoDeletar.addEventListener('click', () =>{
+    //const inputTarefa = document.createElement("input");
+    console.log('oi')
+});
+
+
